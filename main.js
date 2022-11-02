780fa14d4abba23bbe1fe566848d3c3c91499b0a
Fabián Morag <fabian.morag@d2b.cl>
2022-11-02 17:57:25 -0300

    first time random question fixed

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 const API_URL =
   'https://sheet.best/api/sheets/4b800725-403b-432f-852a-e1ae08750dfc'
 
-const JSON_PATH = './data.json'
+const JSON_PATH = 'data.json'
 
 const $ = (e) => document.querySelectorAll(e)
 
 const rand = (size) => Math.floor(Math.random() * size)
 
-;(() => {
-  fetch(JSON_PATH)
-    .then((data) => data.json())
-    .then((json) => sessionStorage.setItem('dataSize', json.length))
-})()
-
-const dataSize = sessionStorage.getItem('dataSize')
-
 const getRandomQuestion = () => {
   return fetch(JSON_PATH)
     .then((res) => res.json())
-    .then((json) => json[rand(dataSize)])
+    .then((json) => json[rand(json.length)])
     .catch((err) => console.log(err))
 }
 
@@ -37,51 +29,6 @@ const getAllQuestions = () => {
 const div = document.createElement('div')
 div.setAttribute('class', 'accordion accordion-flush')
 document.body.append(div)
-
-$('#gen-preg')[0].addEventListener('click', async () => {
-  getRandomQuestion().then((data) => {
-    $('div>ul')[0].innerHTML = ''
-    div.innerHTML = ''
-
-    let { rama, asignatura, concepto } = data
-    sessionStorage.setItem('concepto', concepto)
-
-    let preg = document.createElement('li')
-    preg.classList.add('list-group-item', 'list-group-item-primary')
-    let area = document.createElement('small')
-    area.style.fontSize = '0.55em'
-    let definicion = document.createElement('h3')
-    area.innerHTML = rama + ' - ' + asignatura
-    definicion.innerHTML = concepto.replaceAll('\n', '<br>')
-    preg.append(area, definicion)
-    $('div>ul')[0].append(preg)
-  })
-})
-
-$('#gen-res')[0].addEventListener('click', () => {
-  let concepto = sessionStorage.getItem('concepto')
-  if (!concepto) return
-  sessionStorage.removeItem('concepto')
-
-  getAnswers(concepto).then((data) => {
-    data.forEach((e) => {
-      let definicion = e['definicion']
-      let res = document.createElement('li')
-      res.classList.add('list-group-item', 'list-group-item-success')
-      res.innerHTML = definicion.replaceAll('\n', '<br>')
-      $('div>ul')[0].append(res)
-    })
-  })
-})
-
-$('#gen-todas')[0].addEventListener('click', () => {
-  $('div>ul')[0].innerHTML = ''
-
-  div.setAttribute('id', 'accordionFlushExample')
-  getAllQuestions().then((data) => {
-    data.forEach((e, i) => div.append(createCollapsable(e, i + 1)))
-  })
-})
 
 const createCollapsable = (item, index) => {
   let { id, concepto, definicion } = item
@@ -116,3 +63,49 @@ const createCollapsable = (item, index) => {
 
   return divItem
 }
+
+$('#gen-preg')[0].addEventListener('click', async () => {
+  $('div>ul')[0].innerHTML = ''
+  div.innerHTML = ''
+
+  getRandomQuestion().then((data) => {
+    let { rama, asignatura, concepto } = data
+    sessionStorage.setItem('concepto', concepto)
+
+    let preg = document.createElement('li')
+    preg.classList.add('list-group-item', 'list-group-item-primary')
+    let area = document.createElement('small')
+    area.style.fontSize = '0.55em'
+    let definicion = document.createElement('h3')
+    area.textContent = rama + ' - ' + asignatura
+    definicion.innerHTML = concepto.replaceAll('\n', '<br>')
+    preg.append(area, definicion)
+    $('div>ul')[0].append(preg)
+  })
+})
+
+$('#gen-res')[0].addEventListener('click', () => {
+  let concepto = sessionStorage.getItem('concepto')
+  if (!concepto) return
+  sessionStorage.removeItem('concepto')
+
+  getAnswers(concepto).then((data) => {
+    data.forEach((e) => {
+      let definicion = e['definicion']
+      let res = document.createElement('li')
+      res.classList.add('list-group-item', 'list-group-item-success')
+      res.innerHTML = definicion.replaceAll('\n', '<br>')
+      $('div>ul')[0].append(res)
+    })
+  })
+})
+
+$('#gen-todas')[0].addEventListener('click', () => {
+  $('div>ul')[0].innerHTML = ''
+  if (div.innerHTML !== '') return
+
+  div.setAttribute('id', 'accordionFlushExample')
+  getAllQuestions().then((data) => {
+    data.forEach((e, i) => div.append(createCollapsable(e, i + 1)))
+  })
+})
